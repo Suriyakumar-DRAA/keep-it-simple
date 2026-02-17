c6098c7c0866e6f6f8ae04b6665f2496a4b36f3e
jagadeshjayaraj-git <jagadesh.j@dragarwal.com>
2026-02-17 10:52:01 +0530

    Changes : Changed UI for patient-vertical-journey

--- src/app/features/opd-assessment/pages/patient-journey-vertical/patient-journey-vertical.component.ts
+++ src/app/features/opd-assessment/pages/patient-journey-vertical/patient-journey-vertical.component.ts
@@ -10,7 +10,15 @@ import { AssessmentService } from '@features/opd-assessment/services/assessment.
   styleUrl: './patient-journey-vertical.component.scss'
 })
 export class PatientJourneyVerticalComponent {
-
+   timelineData = [
+    {id:1, label: 'Reception', time: '5 min', status: 'completed', icon: 'assets/images/patient-journey/reception.png'},
+    {id:2, label: 'AR/NCT', time: '15 min', status: 'completed', icon: 'assets/images/patient-journey/ar_nct.png' },
+    {id:3, label: 'Optometrist', time: '10 min', status: 'completed', icon: 'assets/images/patient-journey/optom.png' },
+    {id:4, label: 'Dilation', time: '12 min', status: 'completed', icon: 'assets/images/patient-journey/dilation.png' },
+    {id:5, label: 'Ophthal', time: '12 min', status: 'current', icon: 'assets/images/patient-journey/ophthal.png' },
+    {id:6, label: 'Visit Completed', time: '5 min', status: 'current', icon: 'assets/images/patient-journey/visit_completed.png'},
+    
+  ];
   private assessmentService = inject(AssessmentService);
 
   constructor() { }
