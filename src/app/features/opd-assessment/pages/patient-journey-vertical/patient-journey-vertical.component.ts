854aa2a2705a5184a504b5b40eb4ea8a1f29f5f9
jagadeshjayaraj-git <jagadesh.j@dragarwal.com>
2026-02-17 11:37:06 +0530

    Changes : Changes done in Patient Journey Vertical

--- src/app/features/opd-assessment/pages/patient-journey-vertical/patient-journey-vertical.component.ts
+++ src/app/features/opd-assessment/pages/patient-journey-vertical/patient-journey-vertical.component.ts
@@ -15,7 +15,7 @@ export class PatientJourneyVerticalComponent {
     {id:2, label: 'AR/NCT', time: '15 min', status: 'completed', icon: 'assets/images/patient-journey/ar_nct.png' },
     {id:3, label: 'Optometrist', time: '10 min', status: 'completed', icon: 'assets/images/patient-journey/optom.png' },
     {id:4, label: 'Dilation', time: '12 min', status: 'completed', icon: 'assets/images/patient-journey/dilation.png' },
-    {id:5, label: 'Ophthal', time: '12 min', status: 'current', icon: 'assets/images/patient-journey/ophthal.png' },
+    {id:5, label: 'Ophthalmologist', time: '12 min', status: 'current', icon: 'assets/images/patient-journey/ophthal.png' },
     {id:6, label: 'Visit Completed', time: '5 min', status: 'current', icon: 'assets/images/patient-journey/visit_completed.png'},
     
   ];
