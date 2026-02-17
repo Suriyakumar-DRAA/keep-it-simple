ef114803d4401244db4e40a8eedd93994c1b9123
jagadeshjayaraj-git <jagadesh.j@dragarwal.com>
2026-02-17 12:01:15 +0530

    Changes

--- src/app/features/opd-assessment/pages/patient-journey-vertical/patient-journey-vertical.component.ts
+++ src/app/features/opd-assessment/pages/patient-journey-vertical/patient-journey-vertical.component.ts
@@ -16,7 +16,7 @@ export class PatientJourneyVerticalComponent {
     {id:3, label: 'Optometrist', time: '10 min', status: 'completed', icon: 'assets/images/patient-journey/optom.png' },
     {id:4, label: 'Dilation', time: '12 min', status: 'completed', icon: 'assets/images/patient-journey/dilation.png' },
     {id:5, label: 'Ophthalmologist', time: '12 min', status: 'current', icon: 'assets/images/patient-journey/ophthal.png' },
-    {id:6, label: 'Visit Completed', time: '5 min', status: 'current', icon: 'assets/images/patient-journey/visit_completed.png'},
+    {id:6, label: 'Visit Completed', time: '5 min', status: 'pending', icon: 'assets/images/patient-journey/visit_completed.png'},
     
   ];
   private assessmentService = inject(AssessmentService);
